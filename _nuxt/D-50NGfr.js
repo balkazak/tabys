cb6aa43f17c408c4cbd9055f56c16f5f6bfc4f44
balkazak <nurmukhamed.ibrahim@gmail.com>
2025-03-22 04:39:18 +0600

    Updates

---
+++ _nuxt/D-50NGfr.js
@@ -0,0 +1,1 @@
+const n=()=>{if(typeof window<"u"){const t=window.location.pathname.split("/");if(t.length>1&&t[1]!=="")return"/"+t[1]}return""};export{n as g};
--- _nuxt/nPTScjTt.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./DyHYVYn5.js";const a=s("/images/korean-medical.jpg"),p=s("/images/korean-export.jpg");export{a as _,p as a};
